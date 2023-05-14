ef17d3656ffc95502b1a67a714a382042eda7d3f
VNGhostMans <thanh20057a@gmail.com>
2023-05-14 20:29:12 +0700

    first commit

---
+++ Aura/Protection/Online Strings/OnlineStringClass.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+using System.Reflection;
+
+namespace Aura.Protection.StringOnline
+{
+    internal class OnlineString
+    {
+        public static string Decoder(string encrypted)
+        {
+            if (Assembly.GetExecutingAssembly() != Assembly.GetCallingAssembly()) return "Aura.png";
+            var webClient = new WebClient();
+            return webClient.DownloadString($"https://liria.club/encryption/Decoder.php?string={encrypted}");
+        }
+    }
+}
